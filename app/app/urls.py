d8a4fcf53098f416a6a7a3944c4f74c2859d08c5
Brandonmkmk <brandonxd611@gmail.com>
2024-05-20 11:50:59 -0600

    creando template de home

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
 
@@ -27,5 +27,6 @@ def hello(request,num):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<int:num>',hello, name='hello')
+    path('hello/<int:num>/',hello, name='hello'),
+    path('', include('polls.urls'))
 ]
--- app/polls/views.py
+++ app/polls/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'polls/home.html')
+    questions = Question.objects.all()
+    return render(request, 'polls/home.html',{
+        "questions": questions
+    })
+def vote(request, q_id):
+    pass 
+
+def results(request, q_id):
+    pass
